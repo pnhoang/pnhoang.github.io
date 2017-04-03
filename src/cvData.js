a0b1e134116110d858a7adfa1456df95b9c79d41
Hoang Pham <pnhoang@gmail.com>
2017-04-04 01:02:27 +0200

    edit CV data

--- src/cvData.js
+++ src/cvData.js
@@ -9,7 +9,7 @@ export let personalInfo = [
   },
   {
     key: "Telephone",
-    value: "015205855609",
+    value: "+4915205855609",
   },
   {
     key: "Email",
@@ -20,52 +20,63 @@ export let personalInfo = [
     value: "Vietnamese",
   },
   {
-    key: "Date of birth",
-    value: "12 July 1983",
-  },
-  {
     key: "Gender",
     value: "Male"
   }, 
   {
-    key: "Personal Website",
+    key: "Website",
     value: "http://pnhoang.github.io/"
+  }, 
+  {
+    key: "Availability",
+    value: "Immediate"
   }
-
 ]
 
-export let profile = {
-  key: "Profile",
-  value: "I have more than 7 years experience working as an iOS Developer, 15 years of experience as a Software Programmer, I was in San Francisco for the 2012 Apple World Wide Developer Conference (WWDC) to update my knowledges on iOS development. Since October 2016, I am a Front End Developer working mainly with React JS. My favorite learning platforms are Egghead.io, Udacity, Coursera, iTunes U. "
-}
+export let profile = [
+    "I am an iOS Developer with more than 7 years experience, and 15 years of experience as a Software Programmer, I was in San Francisco for the 2012 Apple World Wide Developer Conference (WWDC) to extend my knowledge in iOS development",
+    "Since October 2016, I have been working as a Front End Developer working with React JS, Javascript, HTML, CSS",
+    "My favorite learning platforms are Egghead.io, Udacity, Coursera, iTunes U",
+]
 
 export let workExperience = [
   {
-    dates: "October 2016 - Now",
-    titles: "Front End Javascript Developer (React)",
-    roles: "Participate in the development of a Bayer project: Qare+ (https://hplus.sparks42.com/) using React JS, HTML, CSS, Javascript, SASS",
-    company: "Sparks42 GmbH, Berlin, Germany"
-  },      
-  {
-    dates: "September 2016 - October 2016",
-    titles: "Front End Developer",
-    roles: "Develop the web application for CASA (https://trycasa.com) using HTML5, CSS3, Javascript, jQuery, Python/Django",
-    company: "Sparks42 GmbH, Berlin, Germany"
-  },      
-  {
-    dates: "January 2016 - September 2016",
-    titles: "iOS  Software Developer",
-    roles: "Develop native iOS app CASA (https://itunes.apple.com/de/app/id1078904776). The application that combines all major furniture companies in Germany, and provides users with social function to share, likes products, discover and search through a large catalog. Languages and Technologies: Objective-C, Swift, UIKit, Core Animation.",
-    company: "Sparks42 GmbH, Berlin, Germany"
+    dates: "January 2016 - Now",
+    titles: [{
+      dates: "October 2016 - Now",
+      title: "Front End Javascript Developer (React)",
+      role: "Develop a major project for Bayer AG: Qare+ (https://hplus.sparks42.com/)",
+      technologies: "React JS, HTML, CSS, Javascript, SASS"
+    },
+    {
+      dates: "September 2016 - October 2016",
+      title: "Front End Developer",
+      role: "Develop the web application for CASA (https://trycasa.com)",
+      technologies: "HTML5, CSS3, Javascript, jQuery, Python/Django"
+    },
+    {
+      dates: "January 2016 - September 2016",
+      title: "iOS  Software Developer",
+      role: "Develop native iOS app CASA (https://itunes.apple.com/de/app/id1078904776). The application that combines all major furniture companies in Germany, and provides users with social function to share, likes products, discover and search through a large catalog",
+      technologies: "Objective-C, Swift, UIKit, Core Animation."
+    }],
+    company: "Sparks42 GmbH, Berlin, Germany",
   },      
   {
     dates: "September 2014 - July 2015",
     titles: "iOS Software Developer",
-    roles: "Develop universal native application TuttoCitta - 3D Maps and Events (https://itunes.apple.com/it/app/id508345696), the application allows users to find local businesses, pharmacies, supermarkets, restaurants, hotels, events, concerts, etc... around a specified location or the current location, and then find the routes to that location and display these routes in 3D map and also shows step by step navigation. Millions of download in Italy. \n Develop universal native application Pagine Gialle (Italian Yellow Pages https://itunes.apple.com/it/app/id316341540). One of the best ranking applications in its category in Italy that are used by million of Italians.",
+    roles: [
+      { role: "Develop universal native application TuttoCitta - 3D Maps and Events (https://itunes.apple.com/it/app/id508345696), the application allows users to find local businesses, pharmacies, supermarkets, restaurants, hotels, events, concerts, etc... around a specified location or the current location, and then find the routes to that location, display them in 3D map and also show step-by-step navigation. Millions of download in Italy",
+      technologies: "Objective-C, UIKit, iOS SDK, In-house 3D Maps SDK, Core Graphics, Core Animation" 
+    },
+      { role: "Develop universal native application Pagine Gialle (Italian Yellow Pages https://itunes.apple.com/it/app/id316341540). One of the best ranking applications in its category in Italy that are used by million of users",
+      technologies: "Objective-C, UIKit, iOS SDK, In-house 3D Maps SDK, Core Graphics, Core Animation"
+      }
+    ],
     company: "Seat Pagine Gialle, Milano, Italia"
   },      
   {
-    dates: "September 2012 - 2014",
+    dates: "September 2012 - August 2014",
     titles: "iOS Software Developer",
     roles: "Develop a music discovery application called FMFinder which allows users to find music based on recommendations and listening habits, data from LastFM, iTunes, YouTube",
     company: "Cosmo Studios, Milano, Italia"
@@ -73,61 +84,63 @@ export let workExperience = [
   {
     dates: "October 2009 - July 2011",
     titles: "iOS Software Developer",
-    roles: "Design and development of iPhone applications: EvolutionTic restaurant order system, UCI Cinemas Italia (https://itunes.apple.com/it/app/id396088854). UCI Cinemas Italia application downloads: about 1 million downloads.",
+    roles: "Design and development of iPhone applications: EvolutionTic restaurant order system, UCI Cinemas Italia (https://itunes.apple.com/it/app/id396088854). Downloads: > 1 million",
     company: "Crea Informatica Srl, Milano, Italia"
   },      
   {
     dates: "May 2009 - August 2009",
     titles: "Front End Developer (Web)",
-    roles: "Participate in the development of the moto.it website using the .net Content Management System Spin\&Go, the internal CMS system.",
+    roles: "Develop moto.it website using the .NET Content Management System Spin\&Go, the internal CMS system",
     company: "Engitel Spa, Milano, Italia"
   },      
   {
     dates: "July 2008 - April 2009",
     titles: ".NET Developer",
-    roles: "Participate in the development of the UGS system for management of loading/unloading cements at the construction site.",
+    roles: "Develop a system for management of loading/unloading materials at construction site",
+    technologies: ".NET, Crystal Reports",
     company: "Italcementi Spa, Bergamo, Italia"
   },      
   {
     dates: "February 2006 - June 2007",
     titles: ".NET Developer",
-    roles: "Participate in the development of Catglobe IT Systems, which is a .net web-based software for doing marketing research.",
+    roles: "Development of Catglobe IT Systems, which is a .NET Web-based software for doing marketing research",
+    technologies: ".NET, SQL",
     company: "Catglobe Vietnam, Ho Chi Minh City, Vietnam"
   },      
   {
-    dates: "2002 - 2005",
+    dates: "December 2002 - December 2005",
     titles: "Software Developer",
-    roles: "Design and development of various websites using ASP.NET and SqlServer 2000. Design and develop some open source softwares: an MPI (Message Passing Interface) application for calculating the shortest path using Moore algorithm, a Pascal-like language compiler, an artificial intelligent mobile game in J2ME, CMSNews module in DotNetNuke",
-    company: ""
+    roles: "Design and development of various websites using ASP.NET and SQLServer 2000, and some open source softwares: an MPI (Message Passing Interface) application for calculating the shortest path using Moore algorithm, a Pascal-like language compiler, an artificial intelligent mobile game in J2ME, CMSNews module in DotNetNuke",
+    company: "Freelance"
   },      
     
 ]
 
 export let skills = [
-  "Ability to learn new languagues and technologies quickly, my favorite learning platforms are Egghead.io, Udacity, Coursera, iTunes U.",
-  "Extensive experience with Objective-C, iOS SDK, Cocoa Touch, Auto Layout, Storyboards, Git, Python.",
-  "Concrete knowledge in React JS, Javascript, HTML, CSS, Bootstrap, jQuery, SASS, LESS, Django, Bottle (Python Web Framework).",
-  "Familiar with C, C++, Bash Script, React Native, Node.js, D3.js, Ruby, Ruby on Rails, Java.",
-  "Personal website written in React.js, React Router, deployed to Github Pages.",
-  "Side projects using Python, Beautiful Soup 4 for web scraping. Another project written in Objective-C and the native iOS SDK for music discovery.",
-  "Capable of working in any environment Mac OS X, Linux, Windows. Favorite editing tool are Xcode, Visual Studio Code. Comfortable editing in vi, TextMate, Latex, Atom, Eclipse.",
-  "Hands-on experience deploying to the clouds: Heroku, Zeit, Github Pages, Google App Engine.", 
-  "Debugging comfortably using Xcode, gcc, gdb, Charles Proxy, Chrome Developer Console.",
-  "Past experiences include .NET, SQL Server, SQLite, DotNetNuke, Eclipse Graphical Modelling Framework, Eclipse Modeling Framework, J2ME.", 
-  "Side interests that helps me on balancing work/life and sometimes help me in professional career include martial art teaching, blogging, video editing",
+  "Ability to learn new languagues and technologies quickly, my favorite learning platforms are Egghead.io, Udacity, Coursera, iTunes U",
+  "Extensive experience with Objective-C, iOS SDK, Cocoa Touch, Auto Layout, Storyboards, Git, Python",
+  "Concrete knowledge in React JS, Javascript, HTML, CSS, Bootstrap, jQuery, SASS, LESS, Django, Bottle (Python Web Framework)",
+  "Familiar with C, C++, Bash Script, React Native, Node.js, D3.js, Ruby, Ruby on Rails, Java",
+  "Side projects using Python, Beautiful Soup 4 for web scraping. Another project written in Objective-C and the native iOS SDK for music discovery",
+  "Capable of working in any environment Mac OS X, Linux, Windows. Favorite editing tool are Xcode, Visual Studio Code. Comfortable editing in vi, TextMate, Latex, Atom, Eclipse",
+  "Hands-on experience deploying to the clouds: Heroku, Zeit, Github Pages, Google App Engine", 
+  "Debugging comfortably using Xcode, gcc, gdb, Charles Proxy, Chrome Developer Console",
+  "Past experiences include .NET, SQL Server, SQLite, DotNetNuke, Eclipse Graphical Modelling Framework, Eclipse Modeling Framework, J2ME",   
 ]
 
 export let education = [
   {
     dates: "December 2011",
     title: "Master of Computer Science",
-    thesis: "Design and development of the editor for the SelfLet environment: SelfLetClipse2",
+    thesis: "Design and develop editor for the SelfLet environment: SelfLetClipse2",
+    grade: "4/4",
     university: "Politecnico di Milano, Milano, Italia",    
   },
   {
     dates: "December 2005",
     title: "Bachelor of Computer Science",
-    thesis: "Design and development of the Content Management System (CMS) for the newspapers publishers",
+    thesis: "Design and develop Content Management System (CMS) for the newspapers publishers",
+    grade: "9/10",
     university: "Ho Chi Minh City University of Technology, Vietnam",    
   },
   
@@ -156,7 +169,7 @@ export let interests = [
   {
     key: "Vovinam Viet Vo Dao",
     value: "I am a trainer of Vovinam Viet Vo Dao ( Vietnamese Martial Art). We train 3 times per week in Friedrichshain and Lichtenberg. http://vovinamberlin.eu "
-  },  
+  },
 ]
 
 export let links = [
